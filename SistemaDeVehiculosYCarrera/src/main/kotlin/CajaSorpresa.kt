487d15bfee22d20bbc2044083e02f8f3645951cf
aarrgom935 <aarrgom935@g.educaand.es>
2024-02-28 01:29:52 +0100

    Clase CajaSorpresa y funcion de llamada en Carrera.

---
+++ SistemaDeVehiculosYCarrera/src/main/kotlin/CajaSorpresa.kt
@@ -0,0 +1,77 @@
+sealed class CajaSorpresa {
+
+    abstract fun accionAplicada(vehiculo: Vehiculo)
+
+    class Sumar10: CajaSorpresa(){
+        override fun accionAplicada(vehiculo: Vehiculo) {
+            println("")
+        }
+
+    }
+
+    class Teletransporte: CajaSorpresa(){
+        override fun accionAplicada(vehiculo: Vehiculo) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    class RetrasarTodos: CajaSorpresa(){
+        override fun accionAplicada(vehiculo: Vehiculo) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    class VehiculoAlInicio: CajaSorpresa(){
+        override fun accionAplicada(vehiculo: Vehiculo) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    class CasillaDeSalida: CajaSorpresa(){
+        override fun accionAplicada(vehiculo: Vehiculo) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    class Restar5: CajaSorpresa(){
+        override fun accionAplicada(vehiculo: Vehiculo) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    class Vacia1: CajaSorpresa(){
+        override fun accionAplicada(vehiculo: Vehiculo) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    class Vacia2: CajaSorpresa(){
+        override fun accionAplicada(vehiculo: Vehiculo) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    class Vacia3: CajaSorpresa(){
+        override fun accionAplicada(vehiculo: Vehiculo) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    class Vacia4: CajaSorpresa(){
+        override fun accionAplicada(vehiculo: Vehiculo) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+
+
+}
